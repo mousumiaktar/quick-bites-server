4f42d09426ebc57c535ab9751b1c8e00dde31627
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-04-25 12:51:14 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+//connection database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayosb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+        await client.connect();
+        const restaurantsCollection = client.db('food_delivery').collection('restaurants');  
+        
+    }
+
+    finally {
+
+    }
+
+
+} run().catch(console.dir);
+
+
+
+
+//check server
+app.get('/', (req, res) => {
+    res.send('server is running')
+});
+
+
+
+//check port
+app.listen(port, () => {
+    console.log("I AM FIRST OPERATION Ridima", port)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
